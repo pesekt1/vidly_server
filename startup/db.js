27fe95712d3fd369c3ea593ba75eafabf3d335c8
Tomas Pesek <pesekt@gmail.com>
2020-04-11 02:15:24 +0200

    updated readme

--- startup/db.js
+++ startup/db.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 const winston = require("winston");
 const config = require("config");
 
-module.exports = function() {
+module.exports = function () {
   //connection to database - its async ... promise
   //we load the connection string dynamically using config.get("db")
   const db = config.get("db");
   mongoose
     .connect(db, {
       useUnifiedTopology: true,
-      useNewUrlParser: true
+      useNewUrlParser: true,
     })
-    .then(() => winston.info(`connected to ${db}...`));
+    .then(() => winston.info(`connected to ${db}`));
 
   //otherwise there is some deprecation warning
   mongoose.set("useCreateIndex", true);
