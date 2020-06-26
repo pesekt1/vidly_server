622c852318dd989cefe1318ea74b714578665df1
Tomas Pesek <pesekt@gmail.com>
2020-06-26 18:55:08 +0200

    connection to heroku clearDB

--- models_mysql/db_mysql.js
+++ models_mysql/db_mysql.js
@@ -1,19 +1,39 @@
 const dbConfig = require("../config_mysql/db.config");
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
+function connect() {
+  let sequelize = {};
+  if (process.env.NODE_ENV == "production") {
+    const db = config.get("mysql_db");
+    sequelize = new Sequelize(db, {
+      dialect: dbConfig.dialect,
+      operatorsAliases: false,
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-});
+      pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle,
+      },
+    });
+  } else {
+    sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+      host: dbConfig.HOST,
+      dialect: dbConfig.dialect,
+      operatorsAliases: false,
 
+      pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle,
+      },
+    });
+  }
+  return sequelize;
+}
+
+const sequelize = connect();
 const db = {};
 
 db.Sequelize = Sequelize;
